37c7e9b4e4ec79be7faab58392fe812760184799
cuongdm250701 <domanhcuong250701@gmail.com>
2024-01-18 11:48:10 -0800

    init source

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+        :id="friend.id"
+        @toggle-favorite="toggleFavorite"
+      ></friend-contact>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      friends: [
+        {
+          id: "1",
+          name: "Manuel Lorenz",
+          phone: "010202012",
+          email: "manuel@gmail.com",
+          isFavorite: true,
+        },
+        {
+          id: "2",
+          name: "July Lorenz",
+          phone: "020320101",
+          email: "july@gmail.com",
+          isFavorite: false,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    toggleFavorite: function (id) {
+      const identified = this.friends.find((item) => item.id === id);
+      identified.isFavorite = !identified.isFavorite;
+    },
+  },
+};
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "Favorite" : "" }}</h2>
+    <button @click="onShowDetail">Show Details</button>
+    <button @click="toggleFavorite(id)">Toggle Favorite</button>
+    <ul v-if="showDetail">
+      <li>{{ phoneNumber }}</li>
+      <li>{{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  props: ["name", "phoneNumber", "emailAddress", "isFavorite", "id"],
+  emits: {
+    "toggle-favorite": function (id) {
+      if (id) {
+        return true;
+      } else {
+        console.error("id is missing");
+      }
+    },
+  },
+  data: function () {
+    return {
+      showDetail: false,
+    };
+  },
+  methods: {
+    onShowDetail: function () {
+      this.showDetail = !this.showDetail;
+    },
+    toggleFavorite: function () {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
+};
+</script>
